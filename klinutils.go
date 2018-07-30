9e63c087fcf94cf23bd1a0bf8fe84e67fbe89009
bgops <bgops@test1.klin-pro.com>
2018-07-30 13:02:50 -0700

    added hostname

--- klinutils.go
+++ klinutils.go
@@ -26,6 +26,33 @@ func Dowork() {
 	time.Sleep(9 * time.Second)
 	fmt.Println("end")
 }
+func Hostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "unknown"
+	}
+
+	addrs, err := net.LookupIP(hostname)
+	if err != nil {
+		return hostname
+	}
+
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			ip, err := ipv4.MarshalText()
+			if err != nil {
+				return hostname
+			}
+			hosts, err := net.LookupAddr(string(ip))
+			if err != nil || len(hosts) == 0 {
+				return hostname
+			}
+			fqdn := hosts[0]
+			return strings.TrimSuffix(fqdn, ".")
+		}
+	}
+	return hostname
+}
 func Joblist(path string) map[string]string {
 	m := make(map[string]string)
 	err := os.Chdir(path)

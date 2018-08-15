4ddcb90a64a1e29ab97a5c89a649d895f9d404d0
bgops <bgops@test1.klin-pro.com>
2018-08-15 15:38:44 -0700

    added wget

---
+++ klinutils_test.go
@@ -0,0 +1,17 @@
+package klinutils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWget(t *testing.T) {
+	fmt.Println("testing wget")
+	w := WgetInfo{
+		Dest:  "util3.klin-pro.com",
+		Dport: "2018",
+		Route: "cacerts/rootca.crt",
+	}
+	b, _ := Wget(w)
+	fmt.Println(string(b))
+}

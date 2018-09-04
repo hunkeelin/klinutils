4682cbd17fda1816304d814e7a1c80fbee39740c
bgops <bgops@test1.klin-pro.com>
2018-09-04 13:56:48 -0700

    added regreq for wget in klinutils

---
+++ netutils.go
@@ -0,0 +1,34 @@
+package klinutils
+
+import (
+	"io/ioutil"
+)
+
+type WgetInfo struct {
+	Dest  string // Destination aka hostname/ip
+	Dport string // The destination port
+	Route string // the route of the file you try to get
+}
+
+func Wget(w WgetInfo) ([]byte, error) {
+	var body []byte
+	var err error
+	j := reqInfo{
+		Dest:               w.Dest,
+		Dport:              w.Dport,
+		TimeOut:            1500,
+		Method:             "GET",
+		Route:              w.Route,
+		InsecureSkipVerify: true,
+	}
+	resp, err := sendPayload(j)
+	if err != nil {
+		return body, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return body, err
+	}
+	return body, err
+}
